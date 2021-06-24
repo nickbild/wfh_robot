f1f672f1573ecdd1b301ac16ec8d21c9df0dca2f
Nick Bild <nick.bild@gmail.com>
2021-06-24 12:34:42 -0400

    Add image server.

--- robot_images.py
+++ robot_images.py
@@ -1,14 +1,5 @@
-import http.server
-import socketserver
-import subprocess
-
-# Start up the camera.
+# Start up the camera in the background.
 subprocess.Popen(["python3", "image_capture_loop.py"])
 
-# Start simple web server (serves 'index.html' by default).
-PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("HTTP server started on port:", PORT)
-    httpd.serve_forever()
+# Start simple web server.
+subprocess.Popen(["python3", "web_server.py"])
---
+++ web_server.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+import subprocess
+
+
+# Start simple web server (serves 'index.html' by default).
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("HTTP server started on port:", PORT)
+    httpd.serve_forever()
