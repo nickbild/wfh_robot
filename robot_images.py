d5435c991497792c5e0fb8f6c6bf837376af3907
Nick Bild <nick.bild@gmail.com>
2021-06-24 12:29:08 -0400

    Add image server.

--- robot_images.py
+++ robot_images.py
@@ -3,7 +3,7 @@ import socketserver
 import subprocess
 
 # Start up the camera.
-subprocess.Popen(["python3 image_capture_loop.py"])
+subprocess.Popen(["python3", "image_capture_loop.py"])
 
 # Start simple web server (serves 'index.html' by default).
 PORT = 8000
