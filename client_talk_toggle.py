00cd62a5d444c647acaafa0df8385339fb6d9684
Nick Bild <nick.bild@gmail.com>
2021-06-26 15:29:38 -0400

    Add script to toggle talk direction.

--- client_talk_toggle.py
+++ client_talk_toggle.py
@@ -8,6 +8,8 @@ PAUSEFILE = '.audio.bot.pause'
 if os.path.isfile(PAUSEFILE):
     os.system("rm -f {0}".format(PAUSEFILE))
     os.system("ssh pi@192.168.1.177 'touch /home/pi/software/wfh_robot/{0}'".format(PAUSEFILE))
+    print("Talk now.")
 else:
     os.system("touch {0}".format(PAUSEFILE))
     os.system("ssh pi@192.168.1.177 'rm -f /home/pi/software/wfh_robot/{0}'".format(PAUSEFILE))
+    print("You're muted.")
